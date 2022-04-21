0b52e6fe20bb3d9b056b2edf51baa04f8755ea97
Logan Ossman <loganossman@gmail.com>
2022-04-21 19:10:51 -0400

    App Skeleton

---
+++ config/colors.js
@@ -0,0 +1,7 @@
+export default {
+    primary: '#E21C1C', /*Top Banner Color*/
+    secondary: '#B01515', /*Bottom button background color */
+    tertiary: '#632A2A', /*Used for background of single files and folders in file home page*/
+    accent: '#E86161', /*Awful pink color that's changing*/
+    buttonColor: '#630C0C', /*Used for the buttons*/
+}
---
+++ screens/LoginScreen.js
@@ -0,0 +1,333 @@
+import { Image, StyleSheet, Text, View, TouchableOpacity, TextInput, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import React from 'react';
+import ImageBackground from 'react-native/Libraries/Image/ImageBackground';
+import { isRequired } from 'react-native/Libraries/DeprecatedPropTypes/DeprecatedColorPropType';
+import colors from '../config/colors.js'
+import { useDeviceOrientation } from '@react-native-community/hooks';
+
+
+export default function LoginScreen({ navigation }) {
+
+  //makes the text boxes have "phantom" text in them by default
+  const [text, onChangeText] = React.useState("Useless Text");
+
+
+    //used to detect device orientation.  If the device is in portrait mode, portrait will be true, else it will be false
+    let {portrait} = useDeviceOrientation();  
+    
+    //defines layout for portrait mode 
+    if (portrait == true)
+    { 
+      return (
+        <TouchableWithoutFeedback onPress={() => {Keyboard.dismiss();}}>
+          <View style={[stylesBase.container, stylesPortrait.container]}>
+
+              {/* Title portion of the page */}
+              <KeyboardAvoidingView behavior='padding'>
+              <View style={[stylesBase.topBorder, stylesPortrait.topBorder]}>
+                  <Text style={stylesBase.titleText}> SparkWorship </Text>
+              </View>
+
+              {/* Container for everything between the buttons panel and the title */}
+              <View style={stylesPortrait.contentContainer}>
+           
+                <View style={stylesPortrait.logoContainter}>
+                  <Image source={require('../assets/icon.png')} style={stylesPortrait.logo}/>
+                </View>
+                
+                <Text style={[stylesBase.titleText, stylesPortrait.centerText]}>Username:</Text>
+                <TextInput style={[stylesPortrait.inputBox, stylesPortrait.centerText]} placeholder="Enter Username Here"/>
+                <Text style={[stylesBase.titleText, stylesPortrait.centerText]}>Password:</Text>
+                <TextInput secureTextEntry={true} style={[stylesPortrait.inputBox, stylesPortrait.centerText]} placeholder="Enter Password Here"/>
+                
+                <TouchableOpacity activeOpacity={1} onPress = {() => navigation.navigate('LoginScreen')} style={[stylesPortrait.button]}>
+                  <View><Text style={{color: "white"}}>Login</Text></View>
+                </TouchableOpacity>
+
+                <TouchableOpacity activeOpacity={1} onPress = {() => navigation.navigate('LoginScreen')}>
+                  <Text style={[stylesPortrait.centerText]}>Register New User</Text>
+                </TouchableOpacity>
+              </View>
+              </KeyboardAvoidingView> 
+          </View>
+          </TouchableWithoutFeedback>
+      );
+    }
+
+    //defines layout for landscape mode 
+    else
+    {
+      return ( 
+        <TouchableWithoutFeedback onPress={() => {Keyboard.dismiss();}}>
+          <View style={[stylesBase.container, stylesLandscape.container]}>
+
+              {/* Title portion of the page */}
+              <KeyboardAvoidingView behavior='height'>
+              <View style={[stylesBase.topBorder, stylesLandscape.topBorder]}>
+                  <Text style={stylesBase.titleText}> SparkWorship </Text>
+              </View>
+
+              {/* Container for everything between the buttons panel and the title */}
+              <View style={stylesLandscape.contentContainer}>
+            
+                
+               
+                {/* 1/3rd columns columns */}
+                <View style={{ flexDirection: 'column', flex:1, margin: 10 }}>
+                  
+                  <View style={{ flexDirection: 'row', flex:1, margin: 1 }}>
+                      <Text style={[stylesBase.titleText, stylesLandscape.centerText]}>Username:</Text>
+                      <TextInput style={[stylesLandscape.inputBox, stylesPortrait.centerText]} placeholder="Enter Username Here"/>
+                      <View style={stylesLandscape.logoContainter}>
+                      <Image source={require('../assets/icon.png')} style={stylesLandscape.logo}/>
+                    </View>
+                      </View>
+
+
+                    <View style={{ flexDirection: 'row', flex:1, margin: 1 }}>
+                    
+                    <Text style={[stylesBase.titleText, stylesLandscape.centerText]}>Password:</Text>
+                    <TextInput secureTextEntry={true} style={[stylesLandscape.inputBoxLong, stylesPortrait.centerText]} placeholder="Enter Password Here"/>
+                    
+                    </View>
+
+                  <TouchableOpacity activeOpacity={1} onPress = {() => navigation.navigate('LoginScreen')} style={[stylesLandscape.button]}>
+                    <View><Text style={{color: "white"}}>Login</Text></View>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity activeOpacity={1} onPress = {() => navigation.navigate('LoginScreen')}>
+                    <Text style={[stylesPortrait.centerText]}>Register</Text>
+                  </TouchableOpacity>  
+
+                </View>      
+              </View>
+                
+              </KeyboardAvoidingView> 
+            </View>
+          </TouchableWithoutFeedback>
+      );
+    }
+    //}
+  
+}
+
+//everything that's different for the portrait layout of the screen
+const stylesPortrait = StyleSheet.create({
+  button:
+  {
+    backgroundColor: colors.buttonColor,
+    marginHorizontal: "10%",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 15,
+    borderColor: "black",
+    height: "7.5%",
+    marginBottom: "10%",
+    borderWidth: 3
+  },
+  inputBox: {
+    height: "7.5%",
+    marginHorizontal: "10%",
+    marginBottom: "10%",
+    borderWidth: 1,
+    borderColor: "black",
+    backgroundColor: "white",
+    paddingLeft: "1%"
+  },
+  centerText:
+  {
+    textAlign: 'center',
+    color: "white"
+  },
+  logoContainter:
+  {
+    marginTop: "5%",
+    marginLeft: "25%",
+    marginBottom: "10%",
+    width: "50%",
+    height: "30%"
+  },
+  logo:
+  {
+    width: "100%",
+    height: "100%"
+  },
+  topBorder:
+  {
+    height: "15%"
+  },
+  buttonsContainer:
+  {
+    width: "100%",
+    height: "12%",
+    flexDirection: "row",
+    backgroundColor: colors.primary
+  },
+  contentContainer:
+  {
+    width: "100%",
+    height: "75%",
+    //backgroundColor: "blue"
+  },
+  file:
+  {
+    width: "100%",
+    height: "10%"
+  },
+  subFile:
+  {
+    width: "100%",
+    height: "10%",
+  }
+  
+}); 
+
+//everything that's different for the landscape layout of the screen
+const stylesLandscape = StyleSheet.create({ 
+  warningText: {
+    marginTop: '0%',
+  },
+  column: {
+    width: '200%',
+    height: '100%'
+  },
+  inputBoxLong: {
+    height: "50%",
+    width: "63%",
+    marginTop: "2%",
+    marginHorizontal: "10%",
+    marginBottom: "5%",
+    borderWidth: 1,
+    borderColor: "black",
+    backgroundColor: "white"
+  },
+  inputBox: {
+    height: "50%",
+    width: "40%",
+    marginHorizontal: "10%",
+    marginBottom: "1%",
+    borderWidth: 1,
+    borderColor: "black",
+    backgroundColor: "white",
+    paddingLeft: "1%"
+  },
+  logoContainter:
+  {
+
+    width: "25%",
+    height: "150%"
+  },
+  logo:
+  {
+    width: "70%",
+    height: "70%"
+  },
+  topBorder:
+  {
+    height: "20%"
+  }, 
+  buttonsContainer:
+  {
+    width: "15%",
+    height: "100%",
+    flexDirection: "column",
+    backgroundColor: colors.secondary
+  },
+  contentContainer:
+  {
+    width: "100%",
+    height: "80%",
+    backgroundColor: "red",
+    flexDirection: "row"
+  },
+  button:
+  {
+    backgroundColor: colors.buttonColor,
+    marginHorizontal: "10%",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 15,
+    borderColor: "black",
+    height: "15%",
+    borderWidth: 3
+  },
+  file:
+  {
+    width: "100%",
+    height: "20%"
+  },
+  subFile:
+  {
+    width: "100%",
+    height: "20%"
+  },
+  fileViewerContainer: 
+  {
+    height: "100%",
+    width: "85%"
+  }
+}); 
+
+//everything that's the same for both portrait and landscape modes
+const stylesBase = StyleSheet.create({
+  container: 
+  {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: colors.secondary
+  },
+  topBorder:
+  {
+    width: "100%",
+    backgroundColor: colors.primary,
+    alignItems: "center",
+    justifyContent: "center"
+    
+  },
+  titleText:
+  {
+      fontSize: 30,
+      /*fontFamily: 'CantoraOne_400Regular',*/
+      color: "white"
+
+  },
+  buttonsContainer:
+  {
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  button:
+  {
+    backgroundColor: colors.buttonColor,
+    marginHorizontal: "10%",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 15,
+    borderColor: "black",
+    height: "7.5%",
+    borderWidth: 3
+  },
+  subFile:
+  {
+    backgroundColor: colors.accent,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "black",
+    borderWidth: 3,
+    flexDirection: "row"
+  },
+  file:
+  {
+    backgroundColor: colors.tertiary,
+    justifyContent: "space-around",
+    alignItems: "center",
+    borderColor: "black",
+    borderWidth: 3,
+    flexDirection: "row"
+  },
+  icon:
+  {
+    backgroundColor: "red"
+  }
+
+});
