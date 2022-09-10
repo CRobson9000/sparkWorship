69c2379e50775995db56e2ed69d28d97e8c267c9
Austin Dorsey <ad1412@messiah.edu>
2022-09-10 16:16:15 -0400

    Merge staging into branch

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+<<<<<<< Updated upstream
 import LoginScreen from './appData/screens/login/LoginScreen.js';
 import DatabaseTest from './appData/screens/test/DatabaseTest.js';
+=======
+import LoginScreen from './appData/screens/LoginScreen.js';
+import DatabaseTest from './appData/screens/DatabaseTest.js';
+import LocationData from './appData/screens/LocationData.js';
+// import {enableLatestRenderer} from 'react-native-maps';
+
+// enableLatestRenderer();
+>>>>>>> Stashed changes
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +21,7 @@ function App() {
         <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="LoginScreen">
           <Stack.Screen name="LoginScreen" component={LoginScreen} />
           <Stack.Screen name="DatabaseTest" component={DatabaseTest} />
+          <Stack.Screen name="LocationData" component={LocationData} />
         </Stack.Navigator>
       </NavigationContainer>
   );
